8eb656aa98459ef6927bc36f9696193481de9232
Emmanuel Emmanuel <imma2005nuel@gmail.com>
2023-05-22 00:40:56 +0100

    add init function for credits

--- js/credits.js
+++ js/credits.js
@@ -1,5 +1,9 @@
 var CreditsController = {
 
-    
+    init: function() {
+        document.getElementById('credits-home').addEventListener('click', function(){
+            app.showView('menu-view');
+        });
+    },
 
 }
