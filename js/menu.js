353b2895ee2d7ae2e31dad0d86f0deffe15d07b4
Emmanuel Emmanuel <imma2005nuel@gmail.com>
2023-05-22 02:02:07 +0100

    add menuController init function for menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,30 @@
 var MenuController = {
-    
+
+    init: function() {
+
+        this.setSoundButton();
+        app.showView('menu-view');
+        
+        document.getElementById('start-game').addEventListener('click', function(){
+            MenuController.startGame();
+        });
+
+        document.getElementById('continue-game').addEventListener('click', function(){
+            MenuController.continueGame();
+        });
+
+        document.getElementById('select-level').addEventListener('click', function(){
+            MenuController.selectLevel(this.level);
+        });
+
+        document.getElementById('load-credits').addEventListener('click', function(){
+            MenuController.loadCredits();
+        });
+
+        document.getElementById('toggle-sound').addEventListener('click', function(){
+            Sound.toggleSound();
+            this.setSoundButton();
+        }.bind(this));
+    },
+
 };
