f9a3dbb3c8deeeb5fb2dc43210b3f485392be1e3
Emmanuel Emmanuel <imma2005nuel@gmail.com>
2023-05-22 13:38:28 +0100

    add init function for puzzle-Animator.js

--- js/libs/puzzle-animator.js
+++ js/libs/puzzle-animator.js
@@ -16,4 +16,21 @@ var puzzleAnimator = {
     maxFrames: 0,
     originalImageFrame: 0,
     paused: false,
+
+    init: function(options) {
+
+        this.clearAnimationData();
+
+        this.options = options;
+        this.options.speed = options.speed || 100;
+
+        this.currentFrame = options.currentFrame || 0;
+        this.maxFrames = options.maxFrames || 1;
+        this.originalImageFrame = (options.originalImageFrame) || 1;
+        this.paused = options.paused || false;
+
+        this.loadImage();
+
+    },
+    
 };
