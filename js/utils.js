a3b865875a57a976e56ae620bea360d7ee787c81
Emmanuel Emmanuel <imma2005nuel@gmail.com>
2023-05-22 02:12:26 +0100

    add hideAllByClass function for utils.js

--- js/utils.js
+++ js/utils.js
@@ -7,4 +7,14 @@ var utils = {
     hideElement: function(elementId) {
         document.getElementById(elementId).style.display = 'none';
     },
+
+    hideAllByClass: function(findClass) {
+        var objects = document.getElementsByClassName(findClass);
+        var i;
+        for (i = 0; i < objects.length; i++) {
+          var targetObject = objects[i];
+          targetObject.style.display = 'none';
+        }
+    },
+
 }
