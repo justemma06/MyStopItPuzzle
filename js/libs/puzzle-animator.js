5fd3ea7e3642f47fe85c0d30d65b987dfb260115
Emmanuel Emmanuel <imma2005nuel@gmail.com>
2023-05-22 13:25:05 +0100

    add img and animation control for puzzle-Animator.js

--- js/libs/puzzle-animator.js
+++ js/libs/puzzle-animator.js
@@ -1,3 +1,19 @@
 var puzzleAnimator = {
-    
+
+    img: null,
+    canvas: null,
+    canvasContext: null,
+    pieces: [],
+    originalPieces: [],
+    pieceWidth: 0,
+    pieceHeight: 0,
+    puzzleWidth: 0,
+    puzzleHeight: 0,
+
+    // Animation Control
+    interval: null,
+    currentFrame: 0,
+    maxFrames: 0,
+    originalImageFrame: 0,
+    paused: false,
 };
