228a0383591b97f09d2cfd8429ff3dab82288d42
Emmanuel Emmanuel <imma2005nuel@gmail.com>
2023-05-22 02:08:35 +0100

    add activeSound function for sound.js

--- js/sound.js
+++ js/sound.js
@@ -3,4 +3,9 @@ var Sound = {
         var actualSound = this.isSoundActive();
         window.localStorage['sound'] = !actualSound;
     },
+
+    isSoundActive: function() {
+        if(localStorage.getItem("sound") === null) return true;
+        return JSON.parse(window.localStorage['sound']);
+    }
 }
