4901f5cfd0beb6680b55f47e46ba0d679fedf041
Emmanuel Emmanuel <imma2005nuel@gmail.com>
2023-05-22 02:11:10 +0100

    add show&hideElements function for utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,10 @@
 var utils = {
-    
+
+    showElement: function(elementId) {
+        document.getElementById(elementId).style.display = 'block';
+    },
+
+    hideElement: function(elementId) {
+        document.getElementById(elementId).style.display = 'none';
+    },
 }
